d371ab6ed44bf0b127d1e4cf52924197adb3025c
CoronetLiu <865753224@qq.com>
2017-09-19 17:47:14 +0800

    JFS

---
+++ login/login.js
@@ -0,0 +1,132 @@
+/*
+* @Author: Marte
+* @Date:   2017-09-14 17:30:55
+* @Last Modified by:   Marte
+* @Last Modified time: 2017-09-18 21:28:42
+*/
+
+// 'use strict';
+$(function(){
+    // console.log(1);
+
+    //加载html
+    $("#top").load("http://10.9.171.178/work/jfs/top.html",function(){
+        $("#code").mouseover(function(){
+            // console.log(this)
+            $(".code").stop().slideDown();
+        })
+        $("#code").mouseout(function(){
+            $(".code").stop().slideUp();
+        })
+    });
+    $("#footer").load("http://10.9.171.178/work/jfs/footer.html",function(){
+        $(".info").mouseover(function(){
+            $(this).stop().animate({
+                width:590,
+            })
+        })
+        $(".info").mouseout(function(){
+            $(this).stop().animate({
+                width:108,
+            })
+        })
+    });
+    $("#section").load("http://10.9.171.178/work/jfs/section.html",function(){
+        $(".weixin").mouseover(function(){
+            $(".erweima").stop().fadeIn();
+        })
+        $(".weixin").mouseout(function(){
+            $(".erweima").stop().fadeOut();
+        })
+    });
+
+
+    //*******************登录********************//
+    $(document).ajaxStart(function(){
+        $("#btn").html("登录中...");
+    })
+    $("#btn").click(function(event){
+        var user = $("input[name=user]").val();
+        var password = $("input[name=password").val();
+        if(user == ""){
+            $(".tips")[0].innerHTML = "用户名不能为空！";
+            $("input[name=user]").css({
+                borderColor:"red"
+            })
+        }else if(password == ""){
+            $(".tips")[0].innerHTML = "";
+            $("input[name=user]").css({
+                borderColor:""
+            });
+            $(".tips")[1].innerHTML = "请输入密码！";
+            $("input[name=password]").css({
+                borderColor:"red"
+            })
+        }else{
+            $(".tips")[1].innerHTML = "";
+            $("input[name=password]").css({
+                borderColor:""
+            });
+
+            $.ajax({
+                url:"http://datainfo.duapp.com/shopdata/userinfo.php",
+                type:"POST",
+                data:{
+                    status:"login",
+                    userID:user,
+                    password:password
+                }
+            })
+            .then(function(res){
+                $("#btn").html("登录");
+                switch(res){
+                    case "0":$(".tips")[0].innerHTML = "用户不存在！";
+                            $("input[name=user]").css({
+                                borderColor:"red"
+                            });
+                            $(".tips")[1].innerHTML = "";
+                            $("input[name=password]").css({
+                                borderColor:""
+                            });
+                            break;
+                    case "2":$(".tips")[1].innerHTML = "密码错误！";
+                            $("input[name=password]").css({
+                                borderColor:"red"
+                            });
+                            $(".tips")[0].innerHTML = "";
+                            $("input[name=user]").css({
+                                borderColor:""
+                            });
+                            break;
+                    default:
+                            $(".tips")[0].innerHTML = "";
+                            $("input[name=user]").css({
+                                borderColor:""
+                            });
+                            $(".tips")[1].innerHTML = "";
+                            $("input[name=password]").css({
+                                borderColor:""
+                            });
+                            $("#loading").css({
+                                display:"block"
+                            })
+                            setTimeout(function(){
+                                window.location.href = "../home/home.html";
+                            },3000)
+                }
+            },function(){
+                alert("未知错误");
+            })
+
+        }
+        //阻止默认事件
+        var e = event || window.event;
+        if(e.preventDefault){
+            e.preventDefault();
+        }else{
+            e.returnValue = false;
+        }
+
+    })
+
+})
---
+++ register/register.js
@@ -0,0 +1,154 @@
+/*
+* @Author: Marte
+* @Date:   2017-09-14 22:54:38
+* @Last Modified by:   Marte
+* @Last Modified time: 2017-09-16 18:26:20
+*/
+
+// 'use strict';
+
+$(function(){
+
+    //加载html
+    $("#top").load("http://10.9.171.178/work/jfs/top.html",function(){
+        $("#code").mouseover(function(){
+            // console.log(this)
+            $(".code").stop().slideDown();
+        })
+        $("#code").mouseout(function(){
+            $(".code").stop().slideUp();
+        })
+    });
+    $("#footer").load("http://10.9.171.178/work/jfs/footer.html",function(){
+        $(".info").mouseover(function(){
+            $(this).stop().animate({
+                width:590,
+            })
+        })
+        $(".info").mouseout(function(){
+            $(this).stop().animate({
+                width:108,
+            })
+        })
+    });
+    $("#section").load("http://10.9.171.178/work/jfs/section.html",function(){
+        $(".weixin").mouseover(function(){
+            $(".erweima").stop().fadeIn();
+        })
+        $(".weixin").mouseout(function(){
+            $(".erweima").stop().fadeOut();
+        })
+    });
+
+    //*******************注册**********************//
+
+    $(document).ajaxStart(function(){
+        $("#btn").attr("value","注册中...")
+    })
+    var index = 0;
+    $("#btn").on("click",function(){
+        $("form").validate();
+        var user = $("input[name=user]").val();
+        var password = $("input[name=password]").val();
+        if(index == 0){//第一次点击
+            index = 1;
+            console.log($("input").hasClass("error"),0);
+            if($("input").hasClass("error")){//如果有错填项
+                console.log(1);
+                $.ajax({
+                    url:"http://datainfo.duapp.com/shopdata/userinfo.php",
+                    data:{
+                        status:"register",
+                        userID:user,
+                        password:password
+                    },
+                    type:"POST"
+                })
+                .then(function(res){
+                    console.log(res)
+                    $("#btn").attr("value","注册");
+                    switch(res){
+                        case "0":
+                                $("label[for=user]").css({
+                                    display:"block"
+                                }).html("用户名已注册")
+                                break;
+                        case "1":
+                                $("label[for=user]").css({
+                                    display:"none"
+                                }).html("");
+                                $("#loading").css({
+                                    display:"block"
+                                });
+                                setTimeout(function(){
+                                    window.location.href = "../home/home.html";
+                                },3000)
+                                break;
+                        case "2":$("#loading").html("服务器炸了！");
+                                break;
+                    }
+                },function(a,b,c){
+                    alert("未知错误！")
+                })//ajax 结束
+                return false;
+            }
+        }else{//不是第一次点击
+            console.log(!$("input").hasClass("error"),1);
+            if(!$("input").hasClass("error")){//如果全部正确
+                console.log(2);
+                $.ajax({
+                    url:"http://datainfo.duapp.com/shopdata/userinfo.php",
+                    data:{
+                        status:"register",
+                        userID:user,
+                        password:password
+                    },
+                    type:"POST"
+                })
+                .then(function(res){
+                    console.log(res)
+                    $("#btn").attr("value","注册");
+                    switch(res){
+                        case "0":
+                                $("label[for=user]").css({
+                                    display:"block"
+                                }).html("用户名已注册")
+                                break;
+                        case "1":
+                                $("label[for=user]").css({
+                                    display:"none"
+                                }).html("");
+                                $("#loading").css({
+                                    display:"block"
+                                });
+                                setTimeout(function(){
+                                    window.location.href = "../home/home.html";
+                                },3000)
+                                break;
+                        case "2":$("#loading").html("服务器炸了！");
+                                break;
+                    }
+                },function(a,b,c){
+                    alert("未知错误！")
+                })//ajax 结束
+                return false;
+            }
+        }//index 判断
+    })
+
+    //随机验证码
+    function codeRandom(){
+        var rcode = "";
+        var str = "1234567890";
+        for(var i = 0;i < 4;i ++){
+            rcode += str[Math.round(Math.random() * (str.length - 1))];
+        }
+        return rcode;
+    }
+    $("#field").attr("value",codeRandom());
+    $("i").on("click",function(){
+        // console.log(1);
+        $("#field").attr("value",codeRandom());
+    })
+
+})
